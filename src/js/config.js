2ff989c46e6a353528c1c89790312c0d368f640c
FireFistDev <akaki7600@gmail.com>
2023-01-15 20:51:54 +0400

    upload project on github

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fe453f85-578e-4e92-8d60-9e4e25de3b46';
+export const MODAL_CLOSE_SEC = 2.5;
